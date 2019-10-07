3daf913b32fbc2701fa93501ff3ddbe316b6b0dc
GHDSawner <55634607+GHDSawner@users.noreply.github.com>
2019-10-07 15:55:39 -0400

    Update main.tf

--- devops-nodejs-startpoint/main.tf
+++ devops-nodejs-startpoint/main.tf
@@ -12,16 +12,4 @@ resource "google_compute_instance" "default" {
       image = "bitnami-jenkins-1-638-1-linux-debian-7-x86-64"
     }
   }
-//  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from Terraform on Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html"
 
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Include this section to give the VM an external ip address
-    }
-  }
-
-  // Apply the firewall rule to allow external IPs to access this instance
- // tags = ["http-server"]
-}
